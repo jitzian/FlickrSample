7bfea7a65219d6a7ebb72fca14453ec38c5fc464
Jonathan Cruz <jitzian@gmail.com>
2019-05-25 23:03:03 -0400

    flkr jc - Wiring ViewModel and Adapter for populating data

--- app/src/main/java/org/com/raian/flickrcodechallenge/ui/viewmodel/DisplayFetchedDataViewModel.kt
+++ app/src/main/java/org/com/raian/flickrcodechallenge/ui/viewmodel/DisplayFetchedDataViewModel.kt
@@ -1,27 +1,32 @@
 package org.com.raian.flickrcodechallenge.ui.viewmodel
 
 import android.content.Context
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.*
 import org.com.raian.flickrcodechallenge.constans.GlobalConstants.Companion.flickrKey
 import org.com.raian.flickrcodechallenge.constans.GlobalConstants.Companion.formatValue
 import org.com.raian.flickrcodechallenge.constans.GlobalConstants.Companion.methodValue
 import org.com.raian.flickrcodechallenge.constans.GlobalConstants.Companion.noJsonCallbackValue
 import org.com.raian.flickrcodechallenge.constans.GlobalConstants.Companion.safeSearchValue
 import org.com.raian.flickrcodechallenge.constans.database.FlickerDataBase
+import org.com.raian.flickrcodechallenge.constans.model.FlickerDataClass
 import org.com.raian.flickrcodechallenge.dependency.injection.components.DaggerComponentInjector
 import org.com.raian.flickrcodechallenge.dependency.injection.modules.NetworkModule
 import org.com.raian.flickrcodechallenge.rest.RestApi
 import org.com.raian.flickrcodechallenge.rest.model.FlickrResultApi
-import retrofit2.Call
-import retrofit2.Callback
+import org.com.raian.flickrcodechallenge.util.safeLet
 import retrofit2.Response
 import retrofit2.Retrofit
 import java.util.logging.Logger
 import javax.inject.Inject
 
 class DisplayFetchedDataViewModel(private val db: FlickerDataBase, private val context: Context) : BaseViewModel() {
+
+    private val listOfDataForUI by lazy {
+        MutableLiveData<List<FlickerDataClass>>()
+    }
+
     @Inject
     lateinit var retrofit: Retrofit
     private val injector = DaggerComponentInjector
@@ -41,7 +46,7 @@ class DisplayFetchedDataViewModel(private val db: FlickerDataBase, private val c
         restApi = retrofit.create(RestApi::class.java)
     }
 
-    fun fetchRemoteData(query: String) = GlobalScope.launch {
+    fun prepareRemoteData(query: String) = GlobalScope.launch(Dispatchers.IO) {
         val queryMap = mutableMapOf(
             "method" to methodValue,
             "api_key" to flickrKey,
@@ -51,19 +56,41 @@ class DisplayFetchedDataViewModel(private val db: FlickerDataBase, private val c
             "text" to query
         )
 
-        val deferredFetchedResult = async {
-            restApi.fetRemoteData(queryMap).enqueue(object : Callback<FlickrResultApi> {
-                override fun onFailure(call: Call<FlickrResultApi>, t: Throwable) {
-                    logger.severe("$TAG::fetchRemoteData::onFailure::${t.message}::${t.cause}")
-                }
-
-                override fun onResponse(call: Call<FlickrResultApi>, response: Response<FlickrResultApi>) {
-                    logger.severe("$TAG::fetchRemoteData::onResponse::${response.body()}")
-                }
-            })
+        val deferredDataFetchedResult = async {
+            restApi.fetRemoteData(queryMap).execute()
         }
 
-        deferredFetchedResult.await()
+        safeLet(deferredDataFetchedResult.await()) { it ->
+            insertDataIntoDb(it)
+        }
+    }
+
+    private fun insertDataIntoDb(response: Response<FlickrResultApi>) = GlobalScope.launch {
+        withContext(Dispatchers.IO) {
+            response.body()?.photos?.photo?.let {
+                for (i in it) {
+                    with(i) {
+                        val innerFlickerDataClass = FlickerDataClass(
+                            id?.toLong(),
+                            owner,
+                            secret,
+                            server,
+                            farm,
+                            title,
+                            ispublic.toString(),
+                            isfriend,
+                            isfamily
+                        )
+                        db.flickerDao().insert(innerFlickerDataClass)
+                    }
+                }
+            }
+            listOfDataForUI.postValue(db.flickerDao().getAll())
+        }
+    }
+
+    fun getListOfDataForUI(): LiveData<List<FlickerDataClass>> {
+        return listOfDataForUI
     }
 
 }
